146e191081f61d081d3a0d43a18097a56f4f8ce7
Alexandr Nikishin <Alexanclr.Nikishin@yandex.ru>
2023-11-27 18:33:14 +0300

    pr13_3

--- models/user.js
+++ models/user.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Поле должно быть заполнено'],
-    minlenght: [2, 'Минимальная длина поля - 2'],
-    maxlenght: [30, 'Максимальная длина подя - 30'],
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   about: {
     type: String,
-    required: [true, 'Поле должно быть заполнено'],
-    minlenght: [2, 'Минимальная длина поля - 2'],
-    maxlenght: [30, 'Максимальная длина подя - 30'],
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   avatar: {
     type: String,
-    required: [true, 'Поле должно быть заполнено'],
+    required: true,
     validate: {
       validator(url) {
         return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(url);
@@ -23,6 +23,6 @@ const userSchema = new mongoose.Schema({
       message: 'Некорректный URL',
     },
   },
-}, { versionKey: false });
+});
 
 module.exports = mongoose.model('user', userSchema);
