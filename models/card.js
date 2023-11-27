747bf718a81bb240584a4b58aecd167d62f90e50
Alexandr Nikishin <Alexanclr.Nikishin@yandex.ru>
2023-11-27 18:35:32 +0300

    pr13_4

--- models/card.js
+++ models/card.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
+    required: [true, 'Поле должно быть заполнено'],
     minlenght: [2, 'Минимальная длина поля - 2'],
     maxlenght: [30, 'Максимальная длина подя - 30'],
   },
   link: {
     type: String,
-    required: [true, 'Поле "link" должно быть заполнено'],
+    required: [true, 'Поле должно быть заполнено'],
     validate: {
       validator(url) {
         return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(url);
